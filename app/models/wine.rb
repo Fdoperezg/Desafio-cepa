af06af781ad05fb23d885709fe6c6b6cc8a2b5e2
Fernando <fp.naldo@gmail.com>
2021-03-19 22:12:44 -0300

    Trying to add strains dinamically with cocoon

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,5 @@
 class Wine < ApplicationRecord
-    has_many :wine_strains
+    has_many :wine_strains, inverse_of: :wine
     has_many :strains, through: :wine_strains, dependent: :destroy
+    accepts_nested_attributes_for :strains, reject_if: :all_blank, allow_destroy: true
 end
